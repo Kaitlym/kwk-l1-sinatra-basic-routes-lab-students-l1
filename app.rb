8ced44870d0bbaa629f55407a7be72f8652e9b9d
Kaitlym <azkatie2014@gmail.com>
2018-06-12 21:22:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/'  do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Kaitlyn"
+  end
+  get '/hometown' do
+    "My hometown is Tolleson"
+  end
+  get '/favorite-song' do
+    "My favorite song is light em up"
+  end
 end
